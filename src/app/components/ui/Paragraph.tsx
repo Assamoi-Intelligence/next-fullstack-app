f0f44bf73fe83c037521c5e8e2578b726d083fb9
Assamoi-Intelligence <56390939+Assamoi-Intelligence@users.noreply.github.com>
2023-09-12 19:15:21 +0000

    commit

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,11 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, HTMLAttributes, forwardRef } from 'react'
+import { VariantProps, cva} from 'class-variance-authority'
 
-interface ParagraphProps {}
+const paragraphVariants = cva("max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center", {
+    variants: {
+        size:  {
+            default: 'text-base sm:text-lg',
+            sm: 'text-sm, sm:text-base'
+        }
+    },
+    defaultVariants: {
+        size: 'default'
+    }
+});
 
-const Paragraph: FC<ParagraphProps> = ({}) => {
-  return (
-    <div>Paragraph</div>
-  )
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof paragraphVariants>{
 }
+
+
+const Paragraph: FC<ParagraphProps> = forwardRef<HTMLParagraphElement, ParagraphProps>(({
+    className, size, children, ...props
+}) => {
+    return <div>¨Paragraph</div>
+});
 
 export default Paragraph
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Paragraph from './components/ui/Paragraph'
 
 export default function Home() {
   return (
-    <main className='bg-red-500'>Hello world</main>
+    <main className='bg-red-500'><Paragraph ref /> </main>
   )
 }
